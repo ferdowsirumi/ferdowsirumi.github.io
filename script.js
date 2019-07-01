7bda5739ee6ea3ae553c726e8663955c8b7e4a16
ferdowsirumi <ahaltaf@gmail.com>
2019-07-01 17:37:10 -0400

    Added files

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 
-
-
 function myfunction() {
   var x = document.getElementById("menubar");
   if (x.className === "topnav") {
@@ -11,3 +9,32 @@ function myfunction() {
 }
 
 
+
+$(window).scroll(function() {
+  $(".slideanim").each(function(){
+    var pos = $(this).offset().top;
+
+    var winTop = $(window).scrollTop();
+    if (pos < winTop + 600) {
+      $(this).addClass("slide");
+    }
+  });
+});
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  showGoToTopButton();
+  //animateBlocks();
+}
+function showGoToTopButton() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 500) {
+    document.getElementById("goToTopBtn").style.display = "block";
+  } else {
+    document.getElementById("goToTopBtn").style.display= "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
